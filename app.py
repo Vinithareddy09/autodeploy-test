d87c0f36146fc6941c4ab225bcd0ca8d8c6860ef
Vinitha Reddy <nallamadhivinitha04@gmail.com>
2025-10-30 09:27:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🎉 AutoDeploy Test Successful! Flask app is running."
+    return "<h2>🚀 AutoDeploy Test Successful!</h2><p>Hello from Vinitha’s Flask App 👋</p>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
